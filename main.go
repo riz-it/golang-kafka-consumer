ef7ee5250971c7458bbf6b1c34dd75f2bfa0d05e
riz.it <mrizalf.email@gmail.com>
2024-07-03 17:48:42 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	config := &kafka.ConfigMap{
+		"bootstrap.servers" : "localhost:9092",
+		"group.id" : "golang",
+		"auto.offset.reset" : "earliest",		
+	}
+
+	consumer, err := kafka.NewConsumer(config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer consumer.Close()
+
+	err = consumer.Subscribe("helloworld", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		message, err := consumer.ReadMessage(1 + time.Second)
+		if err == nil {
+			fmt.Printf("Receive message : %s\n", message.Value)
+		}
+	}
+}
